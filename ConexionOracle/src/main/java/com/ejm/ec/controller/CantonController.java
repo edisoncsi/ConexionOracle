07dde5390397bf25306fcd3063a4fb0a6824757e
Edison Collaguazo <edisoncsi@hotmail.com>
2021-07-22 16:38:08 -0500

    Cambio de orden

--- ConexionOracle/src/main/java/com/ejm/ec/controller/CantonController.java
+++ ConexionOracle/src/main/java/com/ejm/ec/controller/CantonController.java
@@ -1,6 +1,9 @@
 package com.ejm.ec.controller;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -18,6 +21,6 @@ public class CantonController {
 	
 	 @GetMapping("/canton")
 	 private List<Ciudad> getAllCityTest(Model model) {
-	        return ciudadService.getAllCity();
+	        return ciudadService.getAllCity().stream().filter(city -> city.getCod_canton() < 500).collect(Collectors.toList());
 	    }
 }
