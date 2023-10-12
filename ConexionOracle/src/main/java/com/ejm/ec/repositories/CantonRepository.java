852397a3b22b59566ae07873784b84fdee6a98e9
Edison Collaguazo <51208693+edisoncsi@users.noreply.github.com>
2023-10-12 16:06:25 -0500

    Delete ConexionOracle directory

--- ConexionOracle/src/main/java/com/ejm/ec/repositories/CantonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ejm.ec.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejm.ec.model.Ciudad;
-
-public interface CantonRepository extends JpaRepository<Ciudad, Long> {
-
-}
